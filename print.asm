d2aca2f05ba707ed3b7be05c57fc70f1836385b1
Bogdan <bogdan1000leitan2007@gmail.com>
2024-11-28 22:13:48 +0200

    first commit

---
+++ print.asm
@@ -0,0 +1,26 @@
+section .data
+    msg1 db "Salut", 0xA 
+    len1 equ $ - msg1
+    
+    msg2 db "Programez in assembly", 0xA
+    len2 equ $ - msg2
+
+section .text
+    global _start                
+
+_start:
+    mov rax, 1                  
+    mov rdi, 1                  
+    mov rsi, msg1                 
+    mov rdx, len1                
+    syscall                      
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg2
+    mov rdx, len2
+    syscall
+    
+    mov rax, 60                  
+    xor rdi, rdi                 
+    syscall  
